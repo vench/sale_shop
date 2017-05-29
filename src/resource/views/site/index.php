51c47aa591d73289721894009854461fd121dc0b
vench <nowwrit@mail.ru>
2017-05-29 10:58:28 +0300

    Add docker file

--- src/resource/views/site/index.php
+++ src/resource/views/site/index.php
@@ -1,11 +1,17 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$title = $title ? : 'Title page';
 
 ?>
-index
+<html>
+    <head>
+        <title><?php echo $title; ?></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div><h1><?php echo $title; ?> <?php echo mt_rand(); ?></h1></div>
+    </body>
+</html>
+
         
